0c8f9278318fb5dce02bb8d1bf4ebd47912e3a26
Ryan Chang <ryancyq@gmail.com>
2026-04-28 18:08:57 +0800

    ci: add playwright test job

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   reporter: "html",
   use: {
-    baseURL: "http://localhost:5173",
+    baseURL: process.env.CI ? "http://localhost:4173" : "http://localhost:5173",
     trace: "on-first-retry",
   },
   projects: [
@@ -17,8 +17,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173",
+    command: process.env.CI ? "npm run preview" : "npm run dev",
+    url: process.env.CI ? "http://localhost:4173" : "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });
