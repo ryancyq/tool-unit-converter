70c2ecae2dd347a7adef58e5f62803a78c55bc28
Ryan Chang <ryancyq@gmail.com>
2026-04-29 12:47:16 +0800

    fix: workbox fallback should consider base path as well

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,ico,woff,woff2}"],
-        navigateFallback: "/",
+        navigateFallback: base ? `${base}/` : "/",
         navigateFallbackDenylist: [/^\/api\//],
       },
       devOptions: {
